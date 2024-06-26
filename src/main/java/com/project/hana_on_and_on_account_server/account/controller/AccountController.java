3d56f03bb3db0113c4f4755681171af59524add9
Seoha Lee <seohalee0131@gmail.com>
2024-06-26 17:35:46 +0900

    feat: 계좌이체 구현

--- src/main/java/com/project/hana_on_and_on_account_server/account/controller/AccountController.java
+++ src/main/java/com/project/hana_on_and_on_account_server/account/controller/AccountController.java
@@ -1,6 +1,11 @@
 package com.project.hana_on_and_on_account_server.account.controller;
 
+import com.project.hana_on_and_on_account_server.account.dto.AccountDebitRequest;
+import com.project.hana_on_and_on_account_server.account.service.AccountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/accounts")
 public class AccountController {
+
+    private final AccountService accountService;
+
+    @PostMapping("/account-debit")
+    public ResponseEntity<Void> processAccountDebit (@RequestBody AccountDebitRequest accountDebitRequest){
+        accountService.processAccountDebit(accountDebitRequest);
+        return ResponseEntity.noContent().build();
+    }
 }
