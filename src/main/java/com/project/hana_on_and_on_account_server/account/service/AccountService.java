21efee6e16dc6d5d9b8b38ff02ac7b5e130eb2a8
Seoha Lee <seohalee0131@gmail.com>
2024-06-27 09:46:12 +0900

    feat: 계좌 잔액 부족 시 예외 처리 #2

--- src/main/java/com/project/hana_on_and_on_account_server/account/service/AccountService.java
+++ src/main/java/com/project/hana_on_and_on_account_server/account/service/AccountService.java
@@ -3,6 +3,7 @@ package com.project.hana_on_and_on_account_server.account.service;
 import com.project.hana_on_and_on_account_server.account.domain.Account;
 import com.project.hana_on_and_on_account_server.account.domain.AccountTransaction;
 import com.project.hana_on_and_on_account_server.account.domain.AccountTransactionType;
+import com.project.hana_on_and_on_account_server.account.exception.AccountInvalidException;
 import com.project.hana_on_and_on_account_server.account.exception.AccountNotFoundException;
 import com.project.hana_on_and_on_account_server.account.dto.AccountDebitRequest;
 import com.project.hana_on_and_on_account_server.account.repository.AccountRepository;
@@ -25,52 +26,50 @@ public class AccountService {
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void processAccountDebit(AccountDebitRequest accountDebitRequest){
-        try{
-            Account senderAccount = accountRepository.findByAccountNumber(accountDebitRequest.senderAccountNumber())
-                    .orElseThrow(AccountNotFoundException::new);
+        Account senderAccount = accountRepository.findByAccountNumber(accountDebitRequest.senderAccountNumber())
+                .orElseThrow(AccountNotFoundException::new);
 
-            Account receiverAccount = accountRepository.findByAccountNumber(accountDebitRequest.receiverAccountNumber())
-                    .orElseThrow(AccountNotFoundException::new);
+        Account receiverAccount = accountRepository.findByAccountNumber(accountDebitRequest.receiverAccountNumber())
+                .orElseThrow(AccountNotFoundException::new);
 
-
-            AccountTransaction senderAccountTransaction = AccountTransaction.builder()
-                    .accountTransactionTypeCd(AccountTransactionType.TRANSFER.getProperty())
-                    .account(senderAccount)
-                    .senderNm(accountDebitRequest.senderNm())
-                    .senderAccountNumber(accountDebitRequest.senderAccountNumber())
-                    .receiverNm(accountDebitRequest.receiverNm())
-                    .receiverAccountNumber(accountDebitRequest.receiverAccountNumber())
-                    .oldBalance(senderAccount.getBalance())
-                    .newBalance(senderAccount.getBalance() - accountDebitRequest.amount())
-                    .amount(-accountDebitRequest.amount())
-                    .description(accountDebitRequest.description())
-                    .build();
-
-            AccountTransaction receiverAccountTransaction = AccountTransaction.builder()
-                    .accountTransactionTypeCd(AccountTransactionType.TRANSFER.getProperty())
-                    .account(receiverAccount)
-                    .senderNm(accountDebitRequest.senderNm())
-                    .senderAccountNumber(accountDebitRequest.senderAccountNumber())
-                    .receiverNm(accountDebitRequest.receiverNm())
-                    .receiverAccountNumber(accountDebitRequest.receiverAccountNumber())
-                    .oldBalance(receiverAccount.getBalance())
-                    .newBalance(receiverAccount.getBalance() + accountDebitRequest.amount())
-                    .amount(accountDebitRequest.amount())
-                    .description(accountDebitRequest.description())
-                    .build();
-
-            // 계좌 잔액 수정
-            senderAccount.minusAmount(accountDebitRequest.amount());
-            receiverAccount.plusAmount(accountDebitRequest.amount());
-
-            // 계좌 이체 정보 저장
-            accountTransactionRepository.save(senderAccountTransaction);
-            accountTransactionRepository.save(receiverAccountTransaction);
-
-        } catch (ValueInvalidException vie) {
-            //TODO 잔액 부족 시 무시, 추후 미지급 금액에 대한 처리 필요
-            logger.info(vie.getMessage());
+        // sender 계좌 잔액이 부족할 경우 예외 처리
+        if(senderAccount.getBalance() < accountDebitRequest.amount()){
+            new AccountInvalidException();
         }
+
+        AccountTransaction senderAccountTransaction = AccountTransaction.builder()
+                .accountTransactionTypeCd(AccountTransactionType.TRANSFER.getProperty())
+                .account(senderAccount)
+                .senderNm(accountDebitRequest.senderNm())
+                .senderAccountNumber(accountDebitRequest.senderAccountNumber())
+                .receiverNm(accountDebitRequest.receiverNm())
+                .receiverAccountNumber(accountDebitRequest.receiverAccountNumber())
+                .oldBalance(senderAccount.getBalance())
+                .newBalance(senderAccount.getBalance() - accountDebitRequest.amount())
+                .amount(-accountDebitRequest.amount())
+                .description(accountDebitRequest.description())
+                .build();
+
+        AccountTransaction receiverAccountTransaction = AccountTransaction.builder()
+                .accountTransactionTypeCd(AccountTransactionType.TRANSFER.getProperty())
+                .account(receiverAccount)
+                .senderNm(accountDebitRequest.senderNm())
+                .senderAccountNumber(accountDebitRequest.senderAccountNumber())
+                .receiverNm(accountDebitRequest.receiverNm())
+                .receiverAccountNumber(accountDebitRequest.receiverAccountNumber())
+                .oldBalance(receiverAccount.getBalance())
+                .newBalance(receiverAccount.getBalance() + accountDebitRequest.amount())
+                .amount(accountDebitRequest.amount())
+                .description(accountDebitRequest.description())
+                .build();
+
+        // 계좌 잔액 수정
+        senderAccount.minusAmount(accountDebitRequest.amount());
+        receiverAccount.plusAmount(accountDebitRequest.amount());
+
+        // 계좌 이체 정보 저장
+        accountTransactionRepository.save(senderAccountTransaction);
+        accountTransactionRepository.save(receiverAccountTransaction);
     }
 
 
