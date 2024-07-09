4f69829573e1809ea010cb42d43680cef1b4395c
eunjin917 <cgoya01@likelion.org>
2024-07-09 13:39:14 +0900

    feat/#6 CI/CD

--- src/main/java/com/project/hana_on_and_on_account_server/HanaOnAndOnAccountServerApplication.java
+++ src/main/java/com/project/hana_on_and_on_account_server/HanaOnAndOnAccountServerApplication.java
@@ -2,6 +2,7 @@ package com.project.hana_on_and_on_account_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -11,7 +12,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class HanaOnAndOnAccountServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HanaOnAndOnAccountServerApplication.class, args);
+		SpringApplication application = new SpringApplication(HanaOnAndOnAccountServerApplication.class);
+		application.addListeners(new ApplicationPidFileWriter());
+		application.run(args);
 	}
 
 }
